71367dbc3ee005fc38411e5250c204de68fc84a9
Sinaver Idris <sinavercrimea@gmail.com>
2022-12-15 16:45:58 +0400

    initial commit: imp donate func for alchemy university coding exercise

---
+++ README.md
@@ -0,0 +1,4 @@
+## Donate Code Exercise
+
+1. Run `npm install` to install all the depedencies 
+2. Run `node test` to start the test
---
+++ donate.js
@@ -0,0 +1,30 @@
+const { utils, providers, Wallet } = require('ethers');
+const { ganacheProvider } = require('./config');
+
+const provider = new providers.Web3Provider(ganacheProvider);
+
+/**
+ * Donate at least 1 ether from the wallet to each charity
+ * @param   {string} a hex string private key for a wallet with 10 ETH
+ * @param   {array} an array of ethereum charity addresses 
+ *
+ * @returns {Promise} a promise that resolves after all donations have been sent
+ */
+async function donate(privateKey, charities) {
+    const wallet = new Wallet(privateKey, provider);
+    let nonce = await wallet.getTransactionCount();
+
+    const promises = charities.map(async charity => {
+        return wallet.sendTransaction({ 
+            value: utils.parseEther("1"),
+            to: charity, 
+            nonce: nonce++,
+            gasLimit: 21000,
+            gasPrice: utils.parseUnits("5", "gwei")
+        })
+    });
+
+    return Promise.allSettled(promises);
+}
+
+module.exports = donate;
